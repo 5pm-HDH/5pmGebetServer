996afdf05d7870ecbc4dd4f16e97595f50d5e7f3
DumbergerL <lukas.dumberger@gmail.com>
2020-09-04 22:33:20 +0200

    add prayer-list

---
+++ web/static/src/app/prayer-list-item.vue
@@ -0,0 +1,26 @@
+<template>
+    <blockquote>
+    <p><em>{{ prayer }}</em> - {{ date }}</p>
+    </blockquote>
+
+</template>
+
+<script>
+    export default{
+        props: ['data'],
+        data(){
+            return {
+                prayer: (this.data.hasOwnProperty('prayer') ? this.data.prayer : null),
+                date: null,
+                is_public: null,
+                is_approved: null,
+            };
+        },
+        mounted(){
+
+        },
+        methods: {
+            
+        }
+      }
+</script>
--- web/static/src/index.js
+++ web/static/src/index.js
@@ -5,9 +5,15 @@ Vue.prototype.$ajax = ajax;
 
 import example from './app/example.vue';
 import prayerCreateForm from './app/prayer-create-form.vue';
+import prayerList from './app/prayer-list.vue';
+import prayerListItem from './app/prayer-list-item.vue';
+
 
 Vue.component('example', example);
 Vue.component('prayer-create-form', prayerCreateForm);
+Vue.component('prayer-list', prayerList);
+Vue.component('prayer-list-item', prayerListItem);
+
 
 var app = new Vue({
     el: '#app',
