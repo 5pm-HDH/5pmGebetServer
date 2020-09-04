4f3911fb754ee5936b2bfdb32a0f99ebb959499f
DumbergerL <lukas.dumberger@gmail.com>
2020-09-04 22:18:43 +0200

    prayer-create

---
+++ web/static/src/app/prayer-create-form.vue
@@ -0,0 +1,41 @@
+<template>
+    <form>
+    <fieldset>
+        <label for="commentField">Gebet</label>
+        <textarea placeholder="Hi CJ …" id="commentField" v-model="prayer"></textarea>
+        
+        <div class="float-left">
+            <input type="checkbox" id="confirmField" v-model="isPublic">
+            <label class="label-inline" for="confirmField">Öffentlich (anonym) beten</label>
+        </div>
+        <input class="button-primary" value="Senden" v-on:click="dump">
+    </fieldset>
+    </form>
+
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                prayer: null,
+                isPublic: false
+            };
+        },
+        mounted(){
+
+        },
+        methods: {
+            dump: function(){
+               
+                this.$ajax({
+                    method: 'post',
+                    data: this.data
+                }).then( response => {
+                    console.log("RESPONSE", response);
+                });
+
+            }
+        }
+      }
+</script>
--- web/static/src/index.js
+++ web/static/src/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
 import ajax from './ajax';
 
+Vue.prototype.$ajax = ajax;
+
 import example from './app/example.vue';
+import prayerCreateForm from './app/prayer-create-form.vue';
 
 Vue.component('example', example);
+Vue.component('prayer-create-form', prayerCreateForm);
 
 var app = new Vue({
     el: '#app',
