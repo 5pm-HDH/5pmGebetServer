e0a893bc6349df19e54d206abe446737d1dab529
Oliver <oli1111@web.de>
2020-09-13 03:06:50 +0200

    moved database functions to database.go

---
+++ database.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func executeSqlFile(filename string, db *sql.DB) {
+	file, _ := ioutil.ReadFile(filename)
+
+	requests := strings.Split(string(file), ";")
+
+	for _, request := range requests {
+		_, err := db.Exec(request)
+		if err != nil {
+			log.Print(err)
+		}
+	}
+}
+
+func openDatabase() *sql.DB {
+	// check if the sqlite file exists
+	create := true
+	if fileExists("./pray.db") {
+		create = false
+	}
+
+	// open sql connection and database
+	db, err := sql.Open("sqlite3", "./pray.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if create {
+		executeSqlFile("./ddl.sql", db)
+	}
+	return db
+}
--- main.go
+++ main.go
@@ -2,56 +2,10 @@ package main
 
 import (
 	"5pmGebetServer/web"
-	"database/sql"
 	"flag"
-	"fmt"
-	_ "github.com/mattn/go-sqlite3"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"strings"
 )
-
-func fileExists(filename string) bool {
-	info, err := os.Stat(filename)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return !info.IsDir()
-}
-
-func executeSqlFile(filename string, db *sql.DB) {
-	file, _ := ioutil.ReadFile(filename)
-
-	requests := strings.Split(string(file), ";")
-
-	for _, request := range requests {
-		_, err := db.Exec(request)
-		if err != nil {
-			fmt.Print(err)
-		}
-	}
-}
-
-func openDatabase() *sql.DB {
-	// check if the sqlite file exists
-	create := true
-	if fileExists("./pray.db") {
-		create = false
-	}
-
-	// open sql connection and database
-	db, err := sql.Open("sqlite3", "./pray.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if create {
-		executeSqlFile("./ddl.sql", db)
-	}
-	return db
-}
 
 func main() {
 
@@ -59,7 +13,7 @@ func main() {
 	defer db.Close()
 	web.SetDatabase(db)
 
-	port := flag.String("p", "80", "port to serve on")
+	port := flag.String("p", "8080", "port to serve on")
 	directory := flag.String("d", "web/static/", "the directory of static file to host")
 	flag.Parse()
 
