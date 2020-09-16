71f82a83b95baf5b15f9c87a3a18aaf54005d044
DumbergerL <lukas.dumberger@gmail.com>
2020-09-16 09:39:06 +0200

    add test backend and configuration store

--- web/static/src/ajax.js
+++ web/static/src/ajax.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import config from '../config';
 
 const ajax = axios.create({
     timeout: 10000,
-    headers: {'Content-Type': 'application/json'}
+    headers: {'Content-Type': 'application/json'},
+    baseURL: config.base_url
 });
+
 
 export default ajax;
